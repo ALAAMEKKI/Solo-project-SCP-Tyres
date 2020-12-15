697c5c799763f7b5e445eff5c9620af88844713c
ALAAMEKKI <alaamekki01@gmail.com>
2020-12-15 13:50:47 +0100

    init

--- main.js
+++ main.js
@@ -4,14 +4,36 @@ $(document).ready(function(){
 //$('body').append('<div><p>hi</p></div>')
 ////////////////////////////////////////////////////////////////////////////////////////////////
 //slides:
+$('#img2').hide()
+$('#img3').hide()
+$('.contact-body').hide()
+
+
+//Slides:
 var counter = 1;
+var images =['images/1.jpg','images/2.jpg','images/3.jpg','images/4.jpg','images/5.jpg']
 setInterval(function(){
     document.getElementById('radio' + counter).checked = true;
+    $('#img1').attr('src',images[counter-1])
+    $('#img1').css('height', "572.5px")
+
     counter ++
-    if(counter > 3){
+    if(counter > 5){
         counter = 1;
     }
-},5000)
+},3000)
+
+//Navigators:
+$('#home').click(function(){
+    $('.contact-body').hide()
+    $('.slider').show()
+})
+
+$('#contact').click(function(){
+    $('.slider').hide()
+    $('.contact-body').show()
+})
+
 
 
 
